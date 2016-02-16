29c6c2253385da09de34e82c598968187cb74e14
blackhacker666 <saeidzarein@yahoo.com>
2016-02-15 23:32:52 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -217,6 +217,7 @@ function create_config( )
     "anti_spam",
     "owners",
     "arabic_lock",
+    "plugins",
     "set",
     "get",
     "broadcast",
@@ -226,30 +227,10 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {146340607,164853452},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
-]],
+    
     help_text_realm = [[
 Realm Commands:
 
